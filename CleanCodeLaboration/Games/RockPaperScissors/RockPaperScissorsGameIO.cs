7c88f960c05da1145803b88b01b6fcbfefc23746
SintoriSan <sindrielfarsson@gmail.com>
2024-08-28 09:15:41 +0200

    finished the RPS game

--- CleanCodeLaboration/Games/RockPaperScissors/RockPaperScissorsGameIO.cs
+++ CleanCodeLaboration/Games/RockPaperScissors/RockPaperScissorsGameIO.cs
@@ -3,11 +3,9 @@ using CleanCodeLaboration.Interfaces.GameInterfaces;
 
 namespace CleanCodeLaboration.Games.RockPaperScissors;
 
-// Spelets UI.
-
 public class RockPaperScissorsGameIO : IGameIO
 {
-    private const string INVALID_CHOICE = "Invalid input. Please try again.";
+    private const string INVALID_CHOICE_MESSAGE = "Invalid input. Please try again.";
     private const int WINNING_SCORE_DIFFERENCE = 3;
 
     private readonly IGameplayController _controller;
@@ -15,8 +13,8 @@ public class RockPaperScissorsGameIO : IGameIO
     private int _cpuPoints = 0;
     private bool _isPlaying = true;
     private string[] _choices = { "Rock", "Paper", "Scissors" };
-    private int totalMoves => _playerPoints + _cpuPoints;
 
+    private int TotalMoves => _playerPoints + _cpuPoints;
     public IPlayer Player => _controller.GetCurrentPlayer();
     public string ComputerHand => _controller.FirstDataStorage;
     public string PlayerHand => _controller.SecondDataStorage;
@@ -29,28 +27,31 @@ public class RockPaperScissorsGameIO : IGameIO
 
     public void Run()
     {
-        StartGame();
-
-        GameLoop();
-
-        EndGame();
+        try
+        {
+            StartGame();
+            GameLoop();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+        }
+        finally
+        {
+            EndGame();
+        }
     }
 
     private void StartGame()
     {
         GreetPlayer();
-        WelcomeScreen();
         _isPlaying = true;
     }
 
     private void GreetPlayer()
     {
-        Console.WriteLine($"Hello {Player.PlayerId}, and welcome to Rock Paper Scissors!\n");
-        Console.WriteLine("Here are some ground rules for the game!\n" +
-            " -Type in your choice, rock, paper or scissors.\n" +
-            " -To win you have to lead by 3 points.\n" +
-            " -Your score is based on how few tries you can beat the computer\n");
-        Console.WriteLine("To see high scores press 'h' or press any other key to start a new game!");
+        PrintScreen(WelcomeScreen(), false);
+
         var keyPressed = Console.ReadKey();
         if (keyPressed.Key == ConsoleKey.H)
         {
@@ -60,34 +61,9 @@ public class RockPaperScissorsGameIO : IGameIO
         Console.Clear();
     }
 
-    private void WelcomeScreen()
-    {
-        var welcomeScreen =
-@$"
- ___________________________________________
-|                                           |
-| ***************************************** |
-| *                                       * |
-| *      W E L C O M E   T O   T H E      * |
-| *                                       * |
-| *       E P I C  B A T T L E  O F       * |
-| *                                       * |
-| *          R O C K , P A P E R          * |
-| *           S C I S S O R S !           * |
-| ***************************************** |
-|                                           |
-|Prepare yourself for the ultimate showdown!|
-|     Only the strongest will prevail...    |
-|___________________________________________|
-";
-        Console.WriteLine(welcomeScreen);
-        Console.ReadKey();
-        Console.Clear();
-    }
-
     private void GameLoop()
     {
-        PlayingFieldScreen("Round 1");
+        PrintScreen(PlayingFieldScreen("Round 1"), false);
         do
         {
             PrepareOpponent();
@@ -110,11 +86,11 @@ public class RockPaperScissorsGameIO : IGameIO
 
     private string GetValidChoice()
     {
-        string input = Console.ReadLine();
+        var input = Console.ReadLine();
 
         while (!ApprovedChoice(input))
         {
-            Console.WriteLine(INVALID_CHOICE);
+            Console.WriteLine(INVALID_CHOICE_MESSAGE);
             input = Console.ReadLine();
         }
 
@@ -123,10 +99,8 @@ public class RockPaperScissorsGameIO : IGameIO
 
     private bool ApprovedChoice(string input)
     {
-        return _choices
-            .Any(c => c
-            .Equals(input, StringComparison.OrdinalIgnoreCase) ||
-                    input.Equals("q", StringComparison.OrdinalIgnoreCase));
+        return _choices.Any(c => c.Equals(input, StringComparison.OrdinalIgnoreCase) ||
+                                                input.Equals("q", StringComparison.OrdinalIgnoreCase));
     }
 
     private void HandleResults()
@@ -152,14 +126,11 @@ public class RockPaperScissorsGameIO : IGameIO
         }
     }
 
+    private bool Quit() => PlayerHand.Equals("q", StringComparison.OrdinalIgnoreCase);
+
     private bool HasPlayerWon() => (_playerPoints - _cpuPoints) == WINNING_SCORE_DIFFERENCE;
 
     private bool HasCPUWon() => (_cpuPoints - _playerPoints) == WINNING_SCORE_DIFFERENCE;
-
-    private bool Quit()
-    {
-        return PlayerHand.Equals("q", StringComparison.OrdinalIgnoreCase);
-    }
 
     private void DisplayResult()
     {
@@ -169,16 +140,16 @@ public class RockPaperScissorsGameIO : IGameIO
         {
             case -1:
                 _cpuPoints++;
-                PlayingFieldScreen("Computers point!");
+                PrintScreen(PlayingFieldScreen("Computers point!"), false);
                 break;
 
             case 0:
-                PlayingFieldScreen("It's a tie!");
+                PrintScreen(PlayingFieldScreen("It's a tie!"), false);
                 break;
 
             case 1:
                 _playerPoints++;
-                PlayingFieldScreen("Your point!");
+                PrintScreen(PlayingFieldScreen("Your point!"), false);
                 break;
 
             default:
@@ -186,28 +157,11 @@ public class RockPaperScissorsGameIO : IGameIO
         }
     }
 
-    private void PlayingFieldScreen(string? result)
-    {
-        var playingField =
-@$"
-___________________________________________
-|      Player               Computer      |
-|{_playerPoints,10}{_cpuPoints,22}         |
-|                                         |
-|       Hand        VS        Hand        |
-|                                         |
-|{PlayerHand,11}{ComputerHand,22}        |
-|                                         |
-|{result,26}               |
-|_________________________________________|
- Choose you hand: Rock, Paper or Scissors";
-        Console.WriteLine(playingField);
-    }
-
     private void PlayerWinner()
     {
-        DisplayWinnerScreen();
-        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+        PrintScreen(WinnerScreen(), false);
+
+        var keyInfo = Console.ReadKey(true);
         if (keyInfo.Key == ConsoleKey.Y)
         {
             AddHighscore();
@@ -222,41 +176,102 @@ ___________________________________________
 
     private void AddHighscore()
     {
-        _controller.Score = totalMoves;
+        _controller.Score = TotalMoves;
         _controller.CreateAndAddNewHighScore();
     }
 
-    private void DisplayWinnerScreen()
+    private void CPUWinner()
     {
-        var winnerScreen =
-@$"
+        PrintScreen(LoserScreen(), true);
+    }
+
+    private void EndGame()
+    {
+        PrintScreen(GoodbyeScreen(), true);
+        _controller.GameShutDown();
+    }
+
+    private void PrintScreen(string screen, bool interaction)
+    {
+        Console.Clear();
+        Console.WriteLine(screen);
+        if (interaction)
+        {
+            Console.ReadKey();
+        }
+    }
+
+    private string WelcomeScreen()
+    {
+        return @$"
 ___________________________________________
 |                                         |
 |*****************************************|
 |*                                       *|
-|*     C O N G R A T U L A T I O N S     *|
+|*      W E L C O M E   T O   T H E      *|
 |*                                       *|
-|*{Player.PlayerId.ToUpper(),24}               *|
-|*             Y O U   W O N!            *|
+|*       E P I C  B A T T L E  O F       *|
+|*                                       *|
+|*          R O C K , P A P E R          *|
+|*           S C I S S O R S !           *|
+|*****************************************|
+|                                         |
+|{Player.PlayerId,22}!                  |
+| Welcome to the ultimate showdown of     |
+| Rock, Paper, and Scissors!              |
+|                                         |
+| Here are some ground rules for the game:|
+| - Type in your choice: Rock, Paper,     |
+|   or Scissors.                          |
+| - To win, you must lead by 3 points.    |
+| - Your final score is based on how few  |
+|   moves it takes you to beat the        |
+|   computer.                             |
+|                                         |
+| To see high scores press 'h', or press  |
+| any other key to start a new game!      |
+|_________________________________________|
+";
+    }
+
+    private string PlayingFieldScreen(string result)
+    {
+        return
+@$"
+___________________________________________
+|      Player               Computer      |
+|{_playerPoints,10}{_cpuPoints,22}         |
+|                                         |
+|       Hand        VS        Hand        |
+|                                         |
+|{PlayerHand,11}{ComputerHand,22}        |
+|                                         |
+|{result,26}               |
+|_________________________________________|
+ Choose you hand: Rock, Paper or Scissors";
+    }
+
+    private string GoodbyeScreen()
+    {
+        return @$"
+___________________________________________
+|                                         |
+|*****************************************|
+|*                                       *|
+|*       T H A N K   Y O U   F O R       *|
+|*                                       *|
+|*           P L A Y I N G ! !           *|
 |*                                       *|
 |*****************************************|
 |                                         |
-|         Your Score: {totalMoves} points!           |
-|                                         |
-|   Would you like to add your score to   |
-|          the High Score List?           |
-|    Press 'Y' for Yes or any other key   |
-|              to continue...             |
+|              See you soon!              |
 |_________________________________________|
 ";
-
-        Console.Clear();
-        Console.WriteLine(winnerScreen);
     }
 
-    private void DisplayYouLoseScreen()
+    private string LoserScreen()
     {
-        var youLoseScreen =
+        return
     @$"
 ___________________________________________
 |                                         |
@@ -277,37 +292,30 @@ ___________________________________________
 |      Press any key to cry in peace.     |
 |_________________________________________|
 ";
-        Console.Clear();
-        Console.WriteLine(youLoseScreen);
-        Console.ReadKey();
     }
 
-    private void CPUWinner()
+    private string WinnerScreen()
     {
-        DisplayYouLoseScreen();
-        EndGame();
-    }
-
-    private void EndGame()
-    {
-        Console.Clear();
-        var goodbyeScreen =
+        return
 @$"
- ___________________________________________
-|                                           |
-| ***************************************** |
-| *                                       * |
-| *       T H A N K   Y O U   F O R       * |
-| *                                       * |
-| *           P L A Y I N G ! !           * |
-| *                                       * |
-| ***************************************** |
-|                                           |
-|               See you soon!               |
-|___________________________________________|
+___________________________________________
+|                                         |
+|*****************************************|
+|*                                       *|
+|*     C O N G R A T U L A T I O N S     *|
+|*                                       *|
+|*{Player.PlayerId.ToUpper(),24}               *|
+|*             Y O U   W O N!            *|
+|*                                       *|
+|*****************************************|
+|                                         |
+|         Your Score: {TotalMoves} points!           |
+|                                         |
+|   Would you like to add your score to   |
+|          the High Score List?           |
+|    Press 'Y' for Yes or any other key   |
+|              to continue...             |
+|_________________________________________|
 ";
-
-        Console.WriteLine(goodbyeScreen);
-        Console.ReadKey();
     }
 }
